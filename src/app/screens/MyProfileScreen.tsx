ca8232b2d2ec5fd900db1f0ed1c0668c133f98be
Dan Andreea <andan223@gmail.com>
2024-01-05 19:34:58 +0200

    added profile screen

--- src/app/screens/MyProfileScreen.tsx
+++ src/app/screens/MyProfileScreen.tsx
@@ -1,7 +1,113 @@
-import {Text} from 'react-native';
+import {Text, View, StyleSheet, TouchableOpacity} from 'react-native';
+import MyProfileIcon from '../../../assets/svg/MyProfileIcon';
+import {Center} from 'native-base';
 
 const MyProfileScreen = () => {
-  return <Text className="top-16">My Profile</Text>;
+  return (
+    <>
+      <View className="pt-12">
+        <View style={style.banner}></View>
+      </View>
+
+      <View style={style.modalLikeContainer}>
+        <View style={style.titleContainer}>
+          <View style={style.icon}>
+            <MyProfileIcon color="#2E856E" size={25}></MyProfileIcon>
+          </View>
+
+          <View style={style.profileContainer}>
+            <Text style={style.profileName}>Andreea</Text>
+          </View>
+        </View>
+
+        <View style={style.centeredContainer}>
+          <Text style={style.text}>
+            Hi Andreea,{'\n\n'}You have not currently set a goal. Choose one and
+            give us some information about you to begin your health improvement
+            journey!
+          </Text>
+        </View>
+        <View>
+          <TouchableOpacity style={style.logoutButton}>
+            <Text style={style.logoutButtonText}>Logout</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </>
+  );
 };
+
+const style = StyleSheet.create({
+  banner: {
+    paddingTop: 16,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignContent: 'center',
+    paddingBottom: 24,
+  },
+
+  modalLikeContainer: {
+    //className="pt-10 flex-1 rounded-t-[40] bg-white"
+    paddingTop: 10,
+    flex: 1,
+    backgroundColor: 'white',
+    borderTopLeftRadius: 40,
+    borderTopRightRadius: 40,
+  },
+  centeredContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginHorizontal: 30,
+  },
+  text: {
+    fontSize: 16,
+    lineHeight: 22,
+    fontWeight: '500',
+  },
+  profileContainer: {
+    flex: 1,
+    paddingTop: 30,
+    paddingLeft: 15,
+  },
+  profileName: {
+    fontWeight: 'bold',
+    fontSize: 20,
+    color: '#006A4E',
+  },
+  titleContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  titleText: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  icon: {
+    marginTop: 30,
+    marginLeft: 30,
+    backgroundColor: '#EBF7F4',
+    borderRadius: 50,
+    width: 50,
+    height: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  logoutButton: {
+    backgroundColor: '#2E856E',
+    padding: 10,
+    borderRadius: 20,
+    alignItems: 'center',
+    marginBottom: 30,
+    marginLeft: 20,
+    marginRight: 250,
+  },
+  logoutButtonText: {
+    color: 'white',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+});
 
 export default MyProfileScreen;
